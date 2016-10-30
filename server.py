e29155a1e84f0410d60529063ed5bb0fb6418ab8
Harihar Subramanyam <hahasubbu@gmail.com>
2016-10-30 09:07:53 -0400

    Separated websocket functionality into socket_helper

--- server.py
+++ server.py
@@ -1,43 +1,27 @@
-import socketio
-import eventlet
-import eventlet.wsgi
 from flask import Flask
 from flask import render_template
-import json
+from libs.socket_helper import create_socket_server, send_message
 
 PORT = 8000
-sio = socketio.Server()
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!" #HTML goes here.
-
-@app.route("/sankey/")
-def sankey():
+    """
+    The default rout will serve sankey.html.
+    """
     return render_template("sankey.html")
-
-@sio.on('connect', namespace='/')
-def connect(sid, environ):
-    print("connect ", sid)
-    sio.emit("message", json.dumps({"message": "yo"}), room=sid)
-
-@sio.on('message', namespace='/')
-def message(sid, data):
-    print("message ", data)
-    sio.emit('reply', room=sid)
-
-@sio.on('disconnect', namespace='/')
-def disconnect(sid):
-    print('disconnect ', sid)
 
 @app.route("/<path:path>")
 def static_proxy(path):
     """
     Serves static files (e.g. CSS, JS) from static/ directory.
     """
-    return app.wsgi_app.send_static_file(path)
+    return app.send_static_file(path)
 
 if __name__ == "__main__":
-    app = socketio.Middleware(sio, app)
-    eventlet.wsgi.server(eventlet.listen(('', PORT)), app)
+    def dummy_message_handler(message):
+        print("Just got message " + str(message))
+        send_message({"message": "Welcome to the app!"})
+
+    create_socket_server(app, PORT, message_handlers=[dummy_message_handler])
